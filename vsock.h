461edc93cc881a155f628f7b48ddb34b11a84ce9
Slavomir Kaslev <kaslevs@vmware.com>
2018-10-18 11:21:51 +0300

    Removed unnecessary <sys/types.h> header

--- vsock.h
+++ vsock.h
@@ -3,7 +3,6 @@
 
 #include <fcntl.h>
 #include <sys/ioctl.h>
-#include <sys/types.h>
 #include <unistd.h>
 #include <linux/vm_sockets.h>
 
--- vsock_listen.c
+++ vsock_listen.c
@@ -2,7 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/socket.h>
-#include <sys/types.h>
 #include <unistd.h>
 #include <linux/vm_sockets.h>
 
